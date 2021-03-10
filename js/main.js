f9498c0dbbc27a5ab8c614f0055303af4ac51731
Vijay Shukla <vshukla684@gmail.com>
2021-03-10 22:27:55 +0530

    customize the UI of grapesjs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,66 @@
 const editor = grapesjs.init({
   container: "#editor",
-  fromElement: true,
-  width: "auto",
   storageManager: false,
-  plugins: ["gjs-preset-webpage"],
+  blockManager: {
+    appendTo: "#blocks",
+  },
+  styleManager: {
+    appendTo: "#styles-container",
+  },
+  layerManager: {
+    appendTo: "#layers-container",
+  },
+  panels: {
+    defaults: [
+      {
+        id: "basic-actions",
+        el: ".panel__basic-actions",
+        buttons: [
+          {
+            id: "visibility",
+            active: true, // active by default
+            className: "btn-toggle-borders",
+            label: '<i class="bi bi-border"></i>',
+            command: "sw-visibility", // Built-in command
+          },
+        ],
+      },
+      {
+        id: "panel-devices",
+        el: ".panel__devices",
+        buttons: [
+          {
+            id: "device-desktop",
+            label: '<i class="bi bi-laptop"></i>',
+            command: "set-device-desktop",
+            active: true,
+            togglable: false,
+          },
+          {
+            id: "device-mobile",
+            label: '<i class="bi bi-phone"></i>',
+            command: "set-device-mobile",
+            togglable: false,
+          },
+        ],
+      },
+    ],
+  },
+  deviceManager: {
+    devices: [
+      {
+        name: "Desktop",
+        width: "",
+      },
+      {
+        name: "Mobile",
+        width: "320px",
+        widthMedia: "480px",
+      },
+    ],
+  },
+  plugins: ["gjs-blocks-basic"],
   pluginsOpts: {
-    "gjs-preset-webpage": {},
+    "gjs-blocks-basic": {},
   },
 });
